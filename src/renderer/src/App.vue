039fc6381c7f53dbc679ed35d40ee9f06f882c56
DUHUIFENG-PC\admin <duhuifeng813@xmov.ai>
2023-02-11 18:46:22 +0800

    feat: interface achieve

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,8 +4,8 @@ import { ElRow, ElCol, ElLink } from 'element-plus'
 <template>
   <ElRow class="titleBar">
     <ElCol class="titleBarContent" :span="24">
-      <el-image :src="url" style="width: 20px; height: 20px;" class="logo"></el-image>
-      <el-link type="primary" class="titleInfo" :underline="false">有光推流客户端</el-link>
+      <el-image style="width: 20px; height: 20px;" class="logo"></el-image>
+      <el-link type="primary" :underline="false">有光推流客户端</el-link>
     </ElCol>
   </ElRow>
 </template>
@@ -13,22 +13,16 @@ import { ElRow, ElCol, ElLink } from 'element-plus'
 @import './assets/css/styles.less';
 </style>
 
-<script lang="ts">
-export default {
-  data() {
-    return {
-      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
-    }
-  }
-}
-</script>
 <style scoped>
 .el-link {
   color: white;
+  font-size: 12px;
+  margin-top: -10px;
+  margin-left: 10px;
 }
 
 .logo {
-  margin-left: 5 px;
+  margin-left: 15px;
   margin-top: 14px;
 }
 </style>
