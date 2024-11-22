a05093e52359d574ae5d20fbaae6f0e6f89df9b6
huifeng-kooboo <gisdoing@gmail.com>
2024-11-22 16:51:02 +0800

    fix: modify arco ui

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,28 +1,12 @@
-<script setup lang="ts">
-import { ElRow, ElCol, ElLink, ElImage } from 'element-plus'
-</script>
-
 <template>
-  <ElRow class="titleBar">
-    <ElCol class="titleBarContent" :span="24">
-      <el-image  style="width: 20px; height: 20px;" class="logo"  src="https://ytouch-1258011219.cos.ap-nanjing.myqcloud.com/uPic/logo.png" fit="cover" ></el-image>
-      <el-link type="primary" :underline="false">有光推流客户端</el-link>
-    </ElCol>
-  </ElRow>
+  <a-space>
+    <a-button type="primary">Primary</a-button>
+    <a-button>Secondary</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="outline">Outline</a-button>
+    <a-button type="text">Text</a-button>
+  </a-space>
 </template>
 <style lang="less">
 @import './assets/css/styles.less';
-</style>
-<style scoped>
-.el-link {
-  color: white;
-  font-size: 12px;
-  margin-top: -10px;
-  margin-left: 10px;
-}
-
-.logo {
-  margin-left: 15px;
-  margin-top: 14px;
-}
 </style>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,13 +1,10 @@
 // 导入 vue 和 createApp 方法
-import App from "./App.vue";
-import { createApp } from "vue";
-// 导入 element-plus
-import ElementPlus from "element-plus";
-// 导入 element-plus 样式
-import "element-plus/dist/index.css";
+import App from './App.vue'
+import { createApp } from 'vue'
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
 
 // 创建 vue 实例
 const app = createApp(App)
-
-// 使用router\vuex\element-plus并挂载
-app.use(ElementPlus).mount("#app")
+app.use(ArcoVue)
+app.mount('#app')
